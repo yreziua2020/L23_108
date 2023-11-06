5e11d04f50cf21224236c9f6c2fe0a50abe039c7
yreziua2020 <yreziua@gmail.com>
2023-11-06 21:08:23 +0200

    залил 102

--- L23_108.ino
+++ L23_108.ino
@@ -45,8 +45,8 @@ IRsend irsend(kIrLed);
 #define _ZVUK    //чтобы звук отключить закоментровать
 //#define _ip_adr      // непрнятно зачем  без коментарии 102 если закоментировать то 103
 
-#define _ipi 104     //указываем адрес
-#define _getv 42     //указываем шлюз
+#define _ipi 102     //указываем адрес
+#define _getv 199     //указываем шлюз
 IPAddress local_IP(192, 168, 1, _ipi);// Задаем статический IP-адрес:
 IPAddress gateway(192, 168, 1, _getv);// Задаем IP-адрес сетевого шлюза:
 IPAddress primaryDNS(192, 168, 1, _getv);   // опционально
@@ -56,8 +56,6 @@ String weatherHost0 = "api.weatherbit.io";
 IPAddress subnet(255, 255, 255, 0);
 IPAddress secondaryDNS(8, 8, 8, 8); // опционально
 
-//#include <HTTPClient.h>
-//#include <WiFiClientSecure.h>
 
 #include <FS.h>
 #include <ArduinoJson.h>
