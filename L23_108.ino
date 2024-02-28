73e84c0bdf8cfd9feb068421c96d0c289d2259e9
yreziua2020 <yreziua@gmail.com>
2024-02-28 20:42:36 +0200

    англиский

--- L23_108.ino
+++ L23_108.ino
@@ -470,26 +470,49 @@ void loop() {
  
   //if (f_angl &&f_govorit_fraz)  printStringWithShift(tekst[kol_fra_a+1].c_str(), 25);
    if (f_angl && !pr_bip_full && !f_govorit_fraz) {
-/*
+
      switch (f_angl)    
       {
-       case 1:{showSimpleDate(); 
-       
-       
-       break;}
-       case 2:{showSimpleDate(); 
-       
-       break;}
+       case 1:{
+          if (kol_fra_a<26) {
+            printStringWithShift(tekst[kol_fra_a].c_str(), 30);  //play_frazi(1 ,kol_fra_a,6); 
+            play_frazi(1 ,kol_fra_a+1,6); 
+            kol_fra_a++;
+          } else {kol_fra_a=0;   f_angl=2; }
+          //else {kol_fra_a=0;  if(f_angl) { if(f_angl++>3) { f_angl=0;};} 
+        break;}
+       case 2:{
+          if (kol_fra_a<26) {
+          printStringWithShift(tekst[kol_fra_a].c_str(), 30);  //play_frazi(1 ,kol_fra_a,6); 
+          play_frazi(1 ,kol_fra_a+1,6); 
+          kol_fra_a++;}
+          else {kol_fra_a=0;   f_angl=3; }
+          break;}
+        case 3:{
+          if (kol_fra_a<26) {
+          printStringWithShift(tekst[kol_fra_a].c_str(), 30);  //play_frazi(1 ,kol_fra_a,6); 
+          play_frazi(1 ,kol_fra_a+1,6); 
+          kol_fra_a++;}
+          else {kol_fra_a=0;   f_angl=4; }
+          break;}
+        case 4:{
+          if (kol_fra_a<26) {
+          printStringWithShift(tekst[kol_fra_a].c_str(), 30);  //play_frazi(1 ,kol_fra_a,6); 
+          play_frazi(1 ,kol_fra_a+1,6); 
+          kol_fra_a++;}
+          else {kol_fra_a=0;   f_angl=5; }
+          break;}
+         case 5:{
+          if (kol_fra_a<26) {
+          printStringWithShift(tekst[kol_fra_a].c_str(), 30);  //play_frazi(1 ,kol_fra_a,6); 
+          play_frazi(1 ,kol_fra_a+1,6); 
+          kol_fra_a++;}
+          else {kol_fra_a=0;   f_angl=0; }
+          break;}
       }
-*/
-  if (kol_fra_a<26) {
-    printStringWithShift(tekst[kol_fra_a].c_str(), 30);  //play_frazi(1 ,kol_fra_a,6); 
-    play_frazi(1 ,kol_fra_a+1,6); 
-    kol_fra_a++;
-  } else {kol_fra_a=0;  if(f_angl) { if(f_angl++>3) { f_angl=0;};} }
+    
   
-
- }
+ }  //if (f_angl && !pr_bip_full && !f_govorit_fraz) {
 
 
 #endif 
--- perem_mp3.h
+++ perem_mp3.h
@@ -26,6 +26,8 @@ String tekst [] ={" A\222ei\223эй"," B\222bi\222би"," C\222si\222си"," D\2
 
 #ifdef d_104 
 String tekst [] ={"   Aa [ei][эй] "," B [bi:][би:]","  Cc [si:][си:] ","  Dd [di:][ди:] ","    Ee [i:][и:] ","   Ff [ef][эф] ","  Gg [d\227i:]джи:","  Hh[eit\226] эйч","    Ii [ai][ай] "," Jj [d\227ei][джей]","   Kk [kei][кей]","  Ll  [el][эл] " ," Mm  [em][эм] "," Nn  [en][эн] ","  Oo [\224\225][оу]","  Pp [pi:][пи:] "," Qq [kju:]кью:","    Rr [a:][а:] ","  Ss [es][эс] " ,"  Tt [ti:][ти:] ","  Uu [ju:][ю:]","   Vv [vi][ви] "," W[dлblju:]даблью","  Xx [eks] экс" ,"  Yy [wai] уай","  Zz [zed] зед"};
+
+
 #endif 
 uint f_angl;
 uint8_t kol_fra_a;  //количество фраз англиских
